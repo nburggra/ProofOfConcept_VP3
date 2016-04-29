76422d8ecad1beaf33acf0717412173af8604f61
Nick Burggraaff <nburggra@avans.nl>
2016-04-29 10:24:18 +0200

    V2.1.1

--- src/presentation/Panel.java
+++ src/presentation/Panel.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proofofconceptv2;
+package presentation;
 
 /**
  *
  * @author NickB
  */
-public class ProofOfConceptV2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Panel {
     
 }
