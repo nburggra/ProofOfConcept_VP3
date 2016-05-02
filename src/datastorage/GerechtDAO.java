6723fb12ef823780a16d519614dd54bfeca88246
Nick Burggraaff <nburggra@avans.nl>
2016-05-02 13:30:19 +0200

    V2.1.2

---
+++ src/datastorage/GerechtDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastorage;
+
+/**
+ *
+ * @author NickB
+ */
+
+//Data Access Object
+public class GerechtDAO {
+    
+    
+    
+}
